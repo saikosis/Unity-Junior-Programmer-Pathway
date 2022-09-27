a9897da4ffe0072044b53625fd3785bcbe40d245
Sai Sampath Karumudi <sampath.karumudi@gmail.com>
2022-09-27 22:16:11 +0530

    Completed Challenge 2 - Play Fetch

--- Create with Code/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timevar = 0;
 
     // Update is called once per frame
     void Update()
     {
+        if(timevar > 0f)
+        {
+            timevar -= Time.deltaTime;
+        }
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timevar <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timevar = 2f;
         }
     }
 }
--- Create with Code/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Create with Code/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(spawnInterval, 6));
     }
 
     // Spawn random ball at random x position at top of play area
@@ -24,9 +24,10 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int spawnCount = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[spawnCount], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
